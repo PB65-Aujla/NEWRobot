a82072a59e710e9f3e453cae0c631be3147b7822
PB65-Aujla <140618546+PB65-Aujla@users.noreply.github.com>
2024-01-13 14:44:13 +0400

    Update alive.py

--- IRO/modules/alive.py
+++ IRO/modules/alive.py
@@ -10,15 +10,15 @@ from IRO import telethn as tbot
 from IRO.events import register
 
 PHOTO = [
-    "https://telegra.ph/file/4fb072fa4a0c834f8a0f6.jpg",
-    "https://telegra.ph/file/4fb072fa4a0c834f8a0f6.jpg",
+    "https://graph.org/file/211142b4a17856837a5ca.jpg",
+    "https://graph.org/file/cd2c8f5ee35e47b206f6b.jpg",
 ]
 
 
 @register(pattern=("/alive"))
 async def awake(event):
     TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ {dispatcher.bot.first_name}**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀs​ : [Pi͢͢͢kสcђu](https://t.me/{OWNER_USERNAME})\n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀs​ : [PB65AUJLA](https://t.me/{OWNER_USERNAME})\n\n"
     TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
